669a0dd54e34de195faba4bf1dbd77dc01fccbd2
tim <eaz01x@gmail.com>
2021-11-08 17:17:31 +0800

    init project

---
+++ gnar-proxy/src/main/java/top/binaryx/gnar/proxy/GnarProxyApplication.java
@@ -0,0 +1,16 @@
+package top.binaryx.gnar.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tim
+ */
+@SpringBootApplication
+public class GnarProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnarProxyApplication.class, args);
+	}
+
+}
---
+++ gnar-proxy/src/test/java/top/binaryx/gnar/proxy/GnarProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package top.binaryx.gnar.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GnarProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
